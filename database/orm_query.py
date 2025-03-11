9d795cd3cd0f6c025e96d976627dc2abb1053c54
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-03-11 22:27:28 +0300

    Почищен код

--- database/orm_query.py
+++ database/orm_query.py
@@ -4,8 +4,6 @@ from sqlalchemy.orm import joinedload
 
 from database.models import User
 
-# Work with user
-
 async def orm_add_user(
     session: AsyncSession,
     user_id: int,
@@ -16,14 +14,13 @@ async def orm_add_user(
     query = select(User).where(User.user_id == user_id)
     result = await session.execute(query)
     
-    # Проверяем, существует ли пользователь
     if result.scalars().first() is None:
         new_user = User(user_id=user_id, first_name=first_name, last_name=last_name, phone=phone)
         session.add(new_user)
         
-        await session.flush()  # Фиксируем изменения перед коммитом
-        await session.commit()  # Коммитим изменения
+        await session.flush()
+        await session.commit()
         
         return new_user
 
-    return None # Возвращаем None, если пользователь уже существует
+    return None 
