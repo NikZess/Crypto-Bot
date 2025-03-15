73f949ac3eb07f76372421449fc0f784240cf763
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-03-15 22:19:42 +0300

    Добавлены orm запросы на запросы языка

--- database/orm_query.py
+++ database/orm_query.py
@@ -2,7 +2,7 @@ from sqlalchemy import select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
-from database.models import User
+from database.models import User, UserCurrency
 
 async def orm_add_user(
     session: AsyncSession,
@@ -23,10 +23,25 @@ async def orm_add_user(
         
         return new_user
 
-    return None 
+    return None
 
-# async def orm_add_user_currency(
-#     session: AsyncSession,
-#     user_id: int,
-# ):
-#     query = sele
+async def orm_get_user_currency(session: AsyncSession, user_id: int) -> str:
+    stmt = select(UserCurrency.currency).where(UserCurrency.user_id == user_id)
+    
+    result = await session.execute(stmt)
+    currency = result.scalar()
+    
+    return currency if currency else "USDT"
+
+async def orm_set_user_currency(session: AsyncSession, user_id: int, currency: str):
+    stmt = select(UserCurrency).where(UserCurrency.user_id == user_id)
+    
+    result = await session.execute(stmt)
+    user_currency = result.scalar_one_or_none()
+
+    if user_currency:
+        await session.execute(update(UserCurrency).where(UserCurrency.user_id == user_id).values(currency=currency))
+    else:
+        session.add(UserCurrency(user_id=user_id, currency=currency))
+    
+    await session.commit()
