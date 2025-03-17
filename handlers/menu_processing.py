69a53b11d161f46c381d81ff50de74c6f9d2a57c
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-03-17 23:45:26 +0300

    Добавил функцию, оформил код

--- handlers/menu_processing.py
+++ handlers/menu_processing.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from aiogram import types
 from kbds.inline import (
     get_user_main_btns, 
     get_user_prices_btns,
@@ -9,73 +9,60 @@ from kbds.inline import (
     get_user_settings_currency_btns,
     get_user_settings_language_btns,
 )
+from common.text_for_bot import init_descriptions
 
-from common.text_for_bot import description_for_info_pages_russian
-
-async def main_menu(session: AsyncSession, level: float, menu_name: str):
-    description = description_for_info_pages_russian["main_menu"]
+async def main_menu(session: AsyncSession, level: float, menu_name: str, message: types.Message):
+    description = await init_descriptions(message.from_user.id)
     kbds = get_user_main_btns(level=level)
-    
-    return description, kbds
+    return description["main_menu"], kbds  # Возвращаем конкретный текст
 
-async def prices_menu(session: AsyncSession, level: float, menu_name: str):
-    description = description_for_info_pages_russian["prices_menu"]
+async def prices_menu(session: AsyncSession, level: float, menu_name: str, message: types.Message):
+    description = await init_descriptions(message.from_user.id)
     kbds = get_user_prices_btns(level=level)
-    
-    return description, kbds
+    return description["prices_menu"], kbds
 
-async def about_menu(session: AsyncSession, level: float, menu_name: str):
-    description = description_for_info_pages_russian["about_menu"]
+async def about_menu(session: AsyncSession, level: float, menu_name: str, message: types.Message):
+    description = await init_descriptions(message.from_user.id)
     kbds = get_user_about_btns(level=level)
-    
-    return description, kbds
+    return description["about_menu"], kbds
 
-async def settings_menu(session: AsyncSession, level: float, menu_name: str):
-    description = description_for_info_pages_russian["settings"]
+async def settings_menu(session: AsyncSession, level: float, menu_name: str, message: types.Message):
+    description = await init_descriptions(message.from_user.id)
     kbds = get_user_settings_btns(level=level)
-    
-    return description, kbds
+    return description["settings"], kbds
 
 async def settings_menu_market(session: AsyncSession, level: float, menu_name: str):
     description = "<strong>Меню настройки биржи</strong>"
     kbds = get_user_settings_market_btns(level=level)
-    
     return description, kbds
 
-async def settings_menu_currency(session: AsyncSession, level: float, menu_name: str):
-    description = "<strong>Меню настройки валюты бота</strong>"
+async def settings_menu_currency(session: AsyncSession, level: float, menu_name: str, message: types.Message):
+    description = await init_descriptions(message.from_user.id)
     kbds = get_user_settings_currency_btns(level=level)
-    
-    return description, kbds
+    return description["settings_menu_currency"], kbds
 
 async def settings_menu_language(session: AsyncSession, level: float, menu_name: str):
     description = "<strong>Меню настройки языка бота</strong>"
     kbds = get_user_settings_language_btns(level=level)
-
     return description, kbds
 
 async def get_menu_content(
     session: AsyncSession,
     level: int,
     menu_name: str,
+    message: types.Message,
 ):
     if level == 0:
-        return await main_menu(session, level, menu_name)
-    
+        return await main_menu(session, level, menu_name, message)
     if level == 1.0:
-        return await prices_menu(session, level, menu_name)
-    
+        return await prices_menu(session, level, menu_name, message)
     if level == 2.0:
-        return await about_menu(session, level, menu_name)
-    
+        return await about_menu(session, level, menu_name, message)
     if level == 3.0:
-        return await settings_menu(session, level, menu_name)
-    
+        return await settings_menu(session, level, menu_name, message)
     if level == 3.1:
         return await settings_menu_market(session, level, menu_name)
-    
     if level == 3.2:
-        return await settings_menu_currency(session, level, menu_name)
-
+        return await settings_menu_currency(session, level, menu_name, message)
     if level == 3.3:
         return await settings_menu_language(session, level, menu_name)
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,11 +1,11 @@
 import os
 
 from aiogram import Router, types, F, Bot
-from aiogram.filters import CommandStart, Command, or_f
+from aiogram.filters import CommandStart, Command
 
 from kbds.inline import MenuCallBack
 
-from APIparsers.binanceAPI_parsing import get_price
+from APIparsers.currency_market.binanceAPI_parsing import get_price
 
 from filters.chat_type import ChatTypeFilter
 
@@ -16,7 +16,6 @@ from handlers.menu_processing import get_menu_content
 
 from database.models import User
 from sqlalchemy import select
-
 from sqlalchemy.ext.asyncio import AsyncSession
 
 user_private_router = Router()
@@ -24,20 +23,26 @@ user_private_router.message.filter(ChatTypeFilter(["private"]))
 
 @user_private_router.message(CommandStart())
 async def start_cmd_handler(message: types.Message, session: AsyncSession):
-    description, reply_markup = await get_menu_content(session, level=0, menu_name="main")
+    description, reply_markup = await get_menu_content(session, level=0, menu_name="main", message=message)
     
     await message.answer(description, reply_markup=reply_markup)
-    
+
 @user_private_router.callback_query(MenuCallBack.filter())
 async def user_menu(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
     description, reply_markup = await get_menu_content(
         session,
         level=callback_data.level,
-        menu_name=callback_data.menu_name
+        menu_name=callback_data.menu_name,
+        message=callback.message
     )
-    await callback.message.edit_text(description, reply_markup=reply_markup)
-    await callback.answer()
     
+    try:
+        await callback.message.edit_text(description, reply_markup=reply_markup)
+    except Exception as e:
+        await callback.answer("Не удалось обновить меню. Попробуйте позже.")
+
+    await callback.answer()
+
 @user_private_router.message(Command("crypto"))
 async def crypto_cmd_handler(message: types.Message, session: AsyncSession):
     user_id = message.from_user.id
@@ -53,10 +58,29 @@ async def crypto_cmd_handler(message: types.Message, session: AsyncSession):
             text="<strong>Доступные валюты</strong>",
             reply_markup=get_prices_keyboard()
         )
-        
+
 @user_private_router.callback_query(F.data.startswith("usdt"))
-async def get_price_usdt(callback_query: types.CallbackQuery) -> None:
+async def get_price_usdt(callback_query: types.CallbackQuery):
     data = callback_query.data
     currency = data.split("_")[1].upper()
     
-    await callback_query.message.answer(f"Цена {currency} в USDT: {get_price(F"{currency}USDT")}")
+    price = await get_price(f"{currency}USDT")
+    await callback_query.message.answer(f"Цена {currency} в USDT: {price}")
+
+@user_private_router.callback_query(F.data.startswith("currency"))
+async def user_settings_currency_handler(callback_query: types.CallbackQuery, session: AsyncSession):
+    data = callback_query.data
+    user_set_currency = data.split('_')[-1].upper()
+    
+    # Обновляем валюту в базе данных
+    user_id = callback_query.from_user.id
+    query = select(User).where(User.user_id == user_id)
+    result = await session.execute(query)
+    user = result.scalar_one_or_none()
+
+    if user:
+        user.currency = user_set_currency  
+        await session.commit()
+        await callback_query.answer(f"Валюта изменена на {user_set_currency}")
+    else:
+        await callback_query.answer("Ошибка: пользователь не найден")
