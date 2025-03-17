8d7d6b28f2456f05b043ede7bab8833a360a4fb1
Dmitry <157917126+NikZess@users.noreply.github.com>
2025-03-17 23:47:03 +0300

    Добавил подразделение на папки: в одних будут api запросы к биржам, в других парсинг валют (usdt, eur, rub)

--- APIparsers/currency_market/binanceAPI_parsing.py
+++ APIparsers/currency_market/binanceAPI_parsing.py
@@ -1,10 +1,12 @@
 import requests
+from async_lru import alru_cache
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
 }
 
-def get_price(symbol: str):
+@alru_cache
+async def get_price(symbol: str):
     url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
 
     responce = requests.get(url, headers=headers)
